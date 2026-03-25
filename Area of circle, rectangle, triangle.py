1a7b9da6beb18da79ea545392844d8f2f4bc97f4
Priyaranjan133 <abotonabo@gmail.com>
2026-03-25 07:59:53 +0530

    Add area calculation for circle, rectangle, and triangle

---
+++ Area of circle, rectangle, triangle.py
@@ -0,0 +1,15 @@
+import math
+
+# Circle
+r = float(input("Enter radius: "))
+print("Area of Circle:", math.pi * r * r)
+
+# Rectangle
+l = float(input("Enter length: "))
+w = float(input("Enter width: "))
+print("Area of Rectangle:", l * w)
+
+# Triangle
+b = float(input("Enter base: "))
+h = float(input("Enter height: "))
+print("Area of Triangle:", 0.5 * b * h)
